510dc304ab6a46ed7268d2f6b10c295ff791ffbc
Mario Ruiz <mruiznog@xilinx.com>
2022-05-14 13:40:56 +0100

    update button text

--- docs/assets/js/copy_code.js
+++ docs/assets/js/copy_code.js
@@ -7,19 +7,17 @@ copyCodeButtons.forEach((copyCodeButton, index) => {
 
     copyCodeButton.addEventListener('click', () => {
         // Copy the code to the user's clipboard
-        window.navigator.clipboard.writeText(code);
-
-        // Update the button text visually
-        const { innerText: originalText } = copyCodeButton;
-        copyCodeButton.innerText = 'Copied!';
-
-        // (Optional) Toggle a class for styling the button
+        if (code == "" || code == null) {
+            // use text from innerText
+            window.navigator.clipboard.writeText(codeAlt);
+        }
+        else {
+            // use text from textContent
+            window.navigator.clipboard.writeText(code);
+        }
         copyCodeButton.classList.add('copied');
 
         // After 2 seconds, reset the button to its initial UI
-        setTimeout(() => {
-            copyCodeButton.innerText = originalText;
-            copyCodeButton.classList.remove('copied');
-        }, 2000);
+        setTimeout(() => {copyCodeButton.classList.remove('copied');}, 2000);
     });
 });
